6d672009a1edc14023f0186a1c34c02fda966e1e
meenashreyansh12 <142729470+meenashreyansh12@users.noreply.github.com>
2023-09-19 18:28:51 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,6 +3,10 @@ def gitDownload(repo)
     git "https://github.com/intelliqittrainings/${repo}.git"
 }
 def newBuild()
-    {
-      sh "mvn package"
-    }
+{
+     sh "mvn package"
+}
+def newDeploy(jobname,ip,appname)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
